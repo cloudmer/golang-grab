088dc7f27841f500766b28bd727602c90f4139d4
王海波 <644362887@qq.com>
2017-09-23 12:29:25 +0800

    +日志

--- core/algorithm/ssc/contailMultiple.go
+++ core/algorithm/ssc/contailMultiple.go
@@ -400,7 +400,9 @@ func (md *multipleData) calculate() {
 	number := 0
 	strLogHtml := ""
 	status := false
+	newsCode := ""
 	for i := range md.code {
+		newsCode = md.code[i]
 		//检查上一期是否 包含A包
 		pre_code := ""
 		if i != 0 {
@@ -484,7 +486,7 @@ func (md *multipleData) calculate() {
 
 	if md.packet.Id == 8 {
 		log.Println("------------", " - 统计完 - 别名:", md.packet.Alias, "位置", md.position, "AB连续", "彩种:", md.cpTypeName, "报警期数:", strconv.Itoa(number), "计数器", number, "报警期数", md.packet.Number, "当前期是否该报警", status && number >= md.packet.Number)
-		logger.Logs(strLogHtml, md.cpTypeName+"-"+md.position+"-"+md.packet.Alias+"-"+md.code[len(md.code)-1] + "-" + strconv.Itoa(int(time.Now().Unix())))
+		logger.Logs(strLogHtml, md.cpTypeName+"-"+md.position+"-"+md.packet.Alias+"-"+ newsCode + "-" + strconv.Itoa(int(time.Now().Unix())))
 	}
 
 	if status && number >= md.packet.Number {
